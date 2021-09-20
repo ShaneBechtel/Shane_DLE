2d9e8a1d25756dbc5b3bfb2aac12cd87ce8d3943
ShaneBechtel <shane.bechtel@yahoo.com>
2021-09-20 13:47:44 -0700

    Added capability to frame target object without manually setting x/y ranges. Still requires manual wavelength range.

--- DLE_figure_work.py
+++ DLE_figure_work.py
@@ -4,24 +4,27 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 import matplotlib as mpl
 import numpy as np
+from glob import glob
 from pypeit import specobjs
+from pypeit import spec2dobj
 from pypeit.core.coadd import multi_combspec
 from astropy.io import fits
 from pypeit import utils
 from IPython import embed
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--file', help='Spectrum Fits File')
+parser.add_argument('--file_path', help='Coadd directory path')
 parser.add_argument('--redshift', default=0.0, help='Redshift of emission lines')
 parser.add_argument('--blue', help='Exten value for target in Blue Detector')
 parser.add_argument('--red', help='Exten value for target in Blue Detector')
 parser.add_argument('--width', help='Width of boxcar smoothing')
+parser.add_argument('--channel', default=2, help='Which channel to include for 2D image in figure')
 parser.add_argument('--contnorm', default='yes', help='Normalize the continuum flux?')
 args = parser.parse_args()
 
 
 # Example Call
-# python DLE_figure_work.py --file /home/sbechtel/Documents/DEIMOS_Light_Echo/Targets/J1438A/det_all/setup_Both/Science_coadd/spec1d_DE.20190605.30172-DE.20190605.35227-J1438A.fits --redshift 4.95 --blue 9 --red 30 --width 5
+# python DLE_figure_work.py --file /home/sbechtel/Documents/DEIMOS_Light_Echo/Targets/J1438A/det_all/setup_Both/Science_coadd/--redshift 4.95 --blue 9 --red 30 --width 5
 
 def ivarsmooth(flux, ivar, window):
     '''
@@ -58,9 +61,14 @@ def ivarsmooth(flux, ivar, window):
 
 blue_exten = int(args.blue)
 red_exten = int(args.red)
-fits_file = args.file
+file_path=args.file_path
+if file_path[-1] != '/':
+    file_path += '/'
+files = glob(file_path+'*')
+spec1d_file = files[0]
+spec2d_file = files[1]
 
-sobjs = specobjs.SpecObjs.from_fitsfile(fits_file, chk_version=True)
+sobjs = specobjs.SpecObjs.from_fitsfile(spec1d_file, chk_version=True)
 blue_spec = sobjs[blue_exten - 1].to_xspec1d(extraction='OPT', fluxed=True)
 red_spec = sobjs[red_exten - 1].to_xspec1d(extraction='OPT', fluxed=True)
 
@@ -140,12 +148,10 @@ width = int(args.width)
 new_flux, new_ivars = ivarsmooth(new_flux, new_ivars, width)
 new_sig = 1 / np.sqrt(new_ivars)
 
-
 # Quasar Redshifts
 lya = 1215.67
 J14_wave = lya * (1 + 4.709)
 J16_wave = lya * (1 + 3.8101)
-
 
 # Atmospheric Effects
 tell_hdu = fits.open('star_spec_tellcorr.fits')
@@ -157,7 +163,6 @@ flux_corr = new_flux / (tell_corr + (tell_corr == 0))
 ivar_corr = (tell_corr > 0.0) * new_ivars * tell_corr * tell_corr
 mask_corr = (tell_corr > 0.0) * new_masks
 sig_corr = np.sqrt(utils.inverse(ivar_corr))
-
 
 # Composite Spectrum
 comp_file = open('deimos_z4composite.dat')
@@ -176,12 +181,39 @@ comp_waves = comp_data[:, 0]
 comp_flux = comp_data[:, 1]
 comp_flux = (np.median(new_flux) / np.nanmedian(comp_flux)) * comp_flux
 
-
 # 2D Image
-img_hdu = fits.open(
-    '/home/sbechtel/Documents/DEIMOS_Light_Echo/Targets/J1438A/det_all/setup_Both/Science_coadd/spec2d_DE.20190605.30172-DE.20190605.35227-J1438A.fits')
-det = 2
-img_data = img_hdu[(det - 1) * 11 + 4].data
+det = sobjs[blue_exten - 1].DET
+spec2DObj = spec2dobj.Spec2DObj.from_file(spec2d_file, det, chk_version=False)
+channel = int(args.channel)
+if channel == 0:
+    img_data = spec2DObj.sciimg
+    vmax = 15
+    vmin = -3
+elif channel == 1:
+    gpm = spec2DObj.bpmmask == 0
+    img_data = (spec2DObj.sciimg - spec2DObj.skymodel) * gpm
+    vmax = 15
+    vmin = -3
+elif channel == 2:
+    gpm = spec2DObj.bpmmask == 0
+    img_data = (spec2DObj.sciimg - spec2DObj.skymodel) * np.sqrt(spec2DObj.ivarmodel) * gpm
+    vmax = 4
+    vmin = -1
+else:
+    raise ValueError('Expected channel value of 0, 1, or 2')
+
+# Figure Plotting
+img_hdu = fits.open(spec2d_file)
+img_wave = img_hdu[(det - 1) * 11 + 8].data
+wave_low = 7150
+wave_high = 7600
+wave_ind = int(np.round(sobjs[blue_exten - 1].SPAT_PIXPOS))
+spec_low = np.where(img_wave[:, wave_ind] > wave_low)[0][0]
+spec_high = np.where(img_wave[:, wave_ind] < wave_high)[0][-1]
+blue_slit = sobjs[blue_exten - 1].SLITID
+slit_mask = img_hdu[(det - 1) * 11 + 10].data.spat_id == blue_slit
+spat_low = img_hdu[(det - 1) * 11 + 10].data.left_init[slit_mask][0, 0]
+spat_high = img_hdu[(det - 1) * 11 + 10].data.right_init[slit_mask][0, 0]
 
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
@@ -189,9 +221,9 @@ mpl.rcParams['xtick.direction'] = 'in'
 mpl.rcParams['ytick.direction'] = 'in'
 fig, ax = plt.subplots(2, 1, figsize=(20, 16))
 
-ax[0].imshow(img_data.transpose(), vmin=-1, vmax=5)
-ax[0].set_ylim(1380, 1450)
-ax[0].set_xlim(3110, 3670)
+ax[0].imshow(img_data.transpose(), vmin=vmin, vmax=vmax, cmap='gray')
+ax[0].set_ylim(spat_low, spat_high)
+ax[0].set_xlim(spec_low, spec_high)
 ax[0].axes.get_xaxis().set_visible(False)
 ax[0].axes.get_yaxis().set_visible(False)
 
@@ -216,33 +248,16 @@ ax[1].set_xlabel(r'\textbf{Wavelength (\AA)}', size=30)
 ax[1].set_ylabel(r'$$\bf F_{\lambda} \quad (10^{-17} erg s^{-1} cm^{-2} \AA^{-1})$$', size=30)
 ax[1].legend(prop={"size": 20})
 ax[1].set_ylim(-0.02, 0.09)
-ax[1].set_xlim(7150, 7600)
+ax[1].set_xlim(wave_low, wave_high)
 ax[1].xaxis.set_minor_locator(MultipleLocator(10))
 ax[1].yaxis.set_minor_locator(MultipleLocator(0.004))
 ax[1].tick_params('both', length=20, width=2, which='major', labelsize=22)
 ax[1].tick_params('both', length=10, width=1, which='minor')
 ax[0].set_title(r'\textbf{Obj 4219 Spectrum}', size=24)
 plt.tight_layout(h_pad=0)
-plt.subplots_adjust(hspace=-.35)
-#plt.savefig('fig_test.png', bbox_inches='tight')
+plt.subplots_adjust(hspace=-.40)
+plt.savefig('fig_test.png', bbox_inches='tight')
 plt.show()
 plt.close()
 
-# Trying to automatically frame target
-'''
-foo = img_hdu[19].data
-foo_mask = np.ones_like(foo,dtype=bool)
-for i in range(len(foo)):
-    for j in range(len(foo[0])):
-        if (foo[i,j]>7125)&(foo[i,j]<7485):
-            foo_mask[i,j]=False
 
-foo[foo_mask] = 0
-'''
-# det = blue_spec.DET
-# blue_slit = blue_spec.SLITID
-# slit_mask = img_hdu[det*11+10].spat_id == blue_slit
-# left_spat = img_hdu[det*11+10].left_init[slit_mask][0,0]
-# right_spat = img_hdu[det*11+10].right_init[slit_mask][0,0]
-# Can use these to get proper spatial range. Need to manually imput spectral range?
-# How to automatically scale spec image?
