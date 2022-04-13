f6f57e32a3dacd6a12423caf0277091cfcf3acfb
ShaneBechtel <shane.bechtel@yahoo.com>
2022-04-13 11:35:46 -0700

    Fixed minor bugs and generalized to both QSO's

--- transmission_figure.py
+++ transmission_figure.py
@@ -1,21 +1,12 @@
 import argparse
 
 import matplotlib.pyplot as plt
-from matplotlib.ticker import MultipleLocator
 import matplotlib as mpl
 import numpy as np
 from glob import glob
 from pypeit import specobjs
-from pypeit import spec2dobj
-from pypeit import sensfunc
-from pypeit.core.coadd import multi_combspec
-from pypeit.core import flux_calib
 from astropy.io import fits
-from astropy.stats import sigma_clipped_stats
-from pypeit import utils
-from scipy.interpolate import interp1d
 from scipy.optimize import curve_fit
-from pypeit.spectrographs.util import load_spectrograph
 from IPython import embed
 
 
@@ -23,6 +14,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--file_path', help='Coadd directory path')
 parser.add_argument('--exten', help='Exten value for target')
 parser.add_argument('--redshift', default=0.0, help='Redshift of emission lines')
+parser.add_argument('--qso', help='Which Quasar field is object in? (J14 or J16)')
 args = parser.parse_args()
 
 
@@ -57,8 +49,13 @@ new_sig = 1 / np.sqrt(new_ivars)
 
 # Quasar Redshifts
 lya = 1215.67
-J14_wave = lya * (1 + 4.709)
-J16_wave = lya * (1 + 3.8101)
+qso = args.qso
+if qso.lower() == 'j1630':
+    qso_wave = lya * (1 + 3.8101)
+elif qso.lower() == 'j1438':
+    qso_wave = lya * (1 + 4.709)
+else:
+    raise Exception('Please enter a valid QSO option: J1438 or J1630')
 
 # Atmospheric Effects #TODO Don't hard code in this telluric file.
 #tell_hdu = fits.open(tell_file)
@@ -80,8 +77,7 @@ mask_corr = new_masks
 sig_corr = new_sig
 
 
-#TODO Encountering Issue of trying to fit detector flux gap. Need to solve this first. Try hard coding for 8986 for now?
-
+# Currently Hard coded for Obj 8986
 
 z = float(args.redshift)
 
@@ -91,7 +87,7 @@ cont_spec_low = lya_wave + 30.0 #Angstroms
 waves_holder = new_waves[mask_corr]
 cont_spec_ind = np.where(waves_holder >= cont_spec_low)[0][0]
 
-ind_gap_8986 = int(300/0.6)
+ind_gap_8986 = int(300/0.6) #TODO Fix detector flux discontinuity and un-hard code this
 
 def cont_powerlaw(x, a, b, c):
     return a * (x/c)**b
@@ -117,7 +113,7 @@ trans_flux[trans_flux<0.0] = 0.0
 trans_sig = sig_corr[mask_corr]/cont_flux
 
 
-vel_range = 299792.458 * (good_waves-J16_wave)/J16_wave #km/s
+vel_range = 299792.458 * (good_waves-qso_wave)/qso_wave #km/s
 vel_low = -3000
 vel_high = 1500
 
@@ -129,9 +125,9 @@ plt.ylim(-0.1,1.2)
 #plt.show()
 plt.close()
 
+H0 = 67.7
 
-
-dist_range = vel_range / 67.7 #Mpc
+dist_range = vel_range / H0 #Mpc
 
 
 plt.plot(dist_range,trans_flux)
@@ -146,29 +142,38 @@ plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 mpl.rcParams['xtick.direction'] = 'in'
 mpl.rcParams['ytick.direction'] = 'in'
-fig, ax = plt.subplots(figsize=(20,12))
+
+
+fig= plt.figure(figsize=(20,12))
+ax = fig.add_subplot(111,label='1')
+ax2 = fig.add_subplot(111,label='2',frame_on=False)
 
 trans = ax.get_xaxis_transform()
 ax.step(vel_range, trans_flux, 'k', linewidth=1, where='mid', label=r'\textbf{Observed Spectrum}')
-ax.plot(vel_range, trans_sig, 'r:', linewidth=3, label=r'\textbf{Observed Uncertainty}')
-
-#plt.axvline(J14_wave, color='y', linestyle='--', alpha=0.5)
-#plt.text(J14_wave, .85, 'J1438', transform=trans, backgroundcolor='0.75')
-ax.vlines(0,-0.1,1.2, color='y', linestyle='--', alpha=0.5)
-ax.text(0, .9, 'J1630', transform=trans, backgroundcolor='0.75')
-
-ax.set_xlabel(r'\textbf{Velocity (km $$s^{-1}$$)}', size=30)
-ax.set_ylabel(r'$$Ly\alpha$$ Transmission', size=30)
-ax.legend(prop={"size": 20})
+ax.axvline(0, color='y', linestyle='--', alpha=0.5)
+ax.text(0, .9, qso.upper(), transform=trans, backgroundcolor='0.75')
+ax.set_xlabel(r'\textbf{Velocity (km s$^{-1}$)}', size=30)
+ax.set_ylabel(r'\textbf{Transmission}', size=30)
 ax.set_ylim(-0.1,1.2)
 ax.set_xlim(vel_low, vel_high)
-#ax[1].xaxis.set_minor_locator(MultipleLocator(100))
-#ax[1].yaxis.set_minor_locator(MultipleLocator(0.25))
 ax.tick_params('both', length=20, width=2, which='major', labelsize=22)
 ax.tick_params('both', length=10, width=1, which='minor')
-ax.set_title(r'\textbf{(OBJ NAME)}', size=24)
-#plt.savefig('test_figure.png', bbox_inches='tight')
-embed()
+
+
+ax2.plot(dist_range, trans_sig, 'r:', linewidth=3, label=r'\textbf{Observed Uncertainty}')
+ax2.xaxis.tick_top()
+ax2.yaxis.tick_right()
+ax2.set_xlim(vel_low/H0, vel_high/H0)
+ax2.set_xlabel(r'\textbf{Distance (pMpc)}', size=30,labelpad=15)
+ax2.set_ylim(-0.1,1.2)
+ax2.tick_params('both', length=20, width=2, which='major', labelsize=22)
+ax2.tick_params('both', length=10, width=1, which='minor')
+ax2.xaxis.set_label_position('top')
+ax2.yaxis.set_label_position('right')
+ax2.get_yaxis().set_visible(False)
+
+
+plt.savefig('trans_test.png')
 plt.show()
 plt.close()
 
