3f2777a516906519975a27ace7f15e7196d45264
ShaneBechtel <shane.bechtel@yahoo.com>
2022-07-25 11:00:52 -0700

    Updated code to account for simulated QSO/UVB Transmission.

--- transmission_significance.py
+++ transmission_significance.py
@@ -15,16 +15,22 @@ def trans_sig_comp(dist,data,noise_levels,gamma_vals):
 
     data_sig = (data*weights).sum()/N_pix
 
-    sig_vals = (rand.normal(loc=0.0,scale=noise_levels,size=(10000,len(noise_levels)))*weights).sum(axis=1)/N_pix
+    qso_trans = np.exp(-1/gamma_vals)
+
+    sig_vals = ((qso_trans + rand.normal(loc=0.0,scale=noise_levels,size=(10000,len(noise_levels))))*weights).sum(axis=1)/N_pix
     sig_vals.sort()
     y = np.arange(len(sig_vals))/len(sig_vals)
 
-    data_percentile = np.where(data_sig>sig_vals)[0][-1]/len(sig_vals)
 
-    embed()
+    try:
+        data_percentile = np.where(data_sig>sig_vals)[0][-1]/len(sig_vals)
+    except:
+        data_percentile = 0
+
+    embed ()
 
     plt.figure(figsize=(10,7))
-    plt.plot(sig_vals,y,label='Simulated Transmission Enhancement')
-    plt.hlines(data_percentile,sig_vals.min(),sig_vals.max(),label='Data Significance')
+    plt.plot(y,sig_vals,label='Simulated Transmission Enhancement')
+    plt.vlines(data_percentile,sig_vals.min(),sig_vals.max(),label='Data Significance')
     plt.legend()
     plt.show()
