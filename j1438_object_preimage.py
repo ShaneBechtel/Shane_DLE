f0082d4d51b39dc9f52288a6ae11686b6726292e
ShaneBechtel <shane.bechtel@yahoo.com>
2022-08-29 07:57:43 -0700

    Modified scripts to use WCS for coordinates

--- j1438_object_preimage.py
+++ j1438_object_preimage.py
@@ -4,6 +4,7 @@ import matplotlib as mpl
 from astropy.io import fits
 from astropy import units as u
 from astropy.coordinates import SkyCoord
+from astropy.wcs import WCS
 from matplotlib.patches import Circle
 from astropy.stats import sigma_clipped_stats
 from matplotlib.offsetbox import AnchoredText
@@ -14,22 +15,22 @@ from IPython import embed
 mosaic_path = '/home/sbechtel/Downloads/unzip_hold/tobias_code/J1438/deepCoAdd/'
 bands = ['r','i','z']
 
-#obj = '6364'
-obj = '7325'
+obj = '6364'
+#obj = '7325'
 
 if obj == '6364':
-    rband_name = 'calexp-HSC-R2-0-6.6.fits'
-    iband_name = 'calexp-HSC-I2-0-6.6.fits'
-    zband_name = 'calexp-HSC-Z-0-6.6.fits'
-    #coords = '14:38:41.170 +43:06:36.202' #OBJ 6364
-    coords = '14:38:41.2 +43:06:33.5' #OBJ 6364 Shifted manually to nearest object
+    rband_name = 'calexp-HSC-R2-0-5.5.fits'
+    iband_name = 'calexp-HSC-I2-0-5.5.fits'
+    zband_name = 'calexp-HSC-Z-0-5.5.fits'
+    coords = '14:38:41.170 +43:06:36.202' #OBJ 6364
+    #coords = '14:38:41.2 +43:06:33.5' #OBJ 6364 Shifted manually to nearest object
 
 if obj == '7325':
-    rband_name = 'calexp-HSC-R2-0-6.5.fits'
-    iband_name = 'calexp-HSC-I2-0-6.5.fits'
-    zband_name = 'calexp-HSC-Z-0-6.5.fits'
-    #coords = '14:38:42.323 +43:08:57.939' #OBJ 7325
-    coords = '14:38:42.40 +43:08:55.8'  # OBJ 7325 Shifted manually to nearest object
+    rband_name = 'calexp-HSC-R2-0-5.5.fits'
+    iband_name = 'calexp-HSC-I2-0-5.5.fits'
+    zband_name = 'calexp-HSC-Z-0-5.5.fits'
+    coords = '14:38:42.323 +43:08:57.939' #OBJ 7325
+    #coords = '14:38:42.40 +43:08:55.8'  # OBJ 7325 Shifted manually to nearest object
 
 band_paths = [rband_name,iband_name,zband_name]
 
@@ -41,6 +42,14 @@ coords_dec = c.dec.degree
 
 image_fits = fits.open(mosaic_path+band_paths[0])[1]
 
+w = WCS(image_fits.header)
+
+dec_index, ra_index = w.world_to_pixel(c)
+dec_index = int(dec_index)
+ra_index = int(ra_index)
+
+
+'''
 ra_ref_val = image_fits.header['CRVAL1']
 ra_ref_pix = image_fits.header['CRPIX1']
 ra_scale = image_fits.header['CD1_1']
@@ -56,14 +65,17 @@ dec_cen_val = (dec_ref_pix - (size/2))*dec_scale + dec_ref_val
 
 
 ra_pixel_offset = (coords_ra - ra_cen_val)/ra_scale
-ra_index = int(np.round((size/2) + ra_pixel_offset))
+test_ra_index = int(np.round((size/2) + ra_pixel_offset))
 
 dec_pixel_offset = (coords_dec - dec_cen_val)/dec_scale
-dec_index = int(np.round((size/2) + dec_pixel_offset))
+test_dec_index = int(np.round((size/2) + dec_pixel_offset))
+'''
 
+pix_scale = image_fits.header['CD1_1']
+pix_arc = -3600*pix_scale
 
-image_width = 30 #pixels
-
+image_width = int(np.round(5/pix_arc)) # Create images with width 5"
+embed()
 mpl.rcParams['axes.linewidth'] = 5
 
 fig, ax = plt.subplots(1, 3,sharex=True, sharey=True,figsize=(15,5))
--- obj_patch_finder.py
+++ obj_patch_finder.py
@@ -2,6 +2,7 @@ import numpy as np
 from astropy.io import fits
 from astropy import units as u
 from astropy.coordinates import SkyCoord
+from astropy.wcs import WCS
 from glob import glob
 from IPython import embed
 
@@ -10,7 +11,8 @@ patch_files = 'calexp-HSC-R2*.fits'
 
 files = glob(path+patch_files)
 
-coords = '14:38:42.323 +43:08:57.939' #OBJ 7325
+coords = '14:38:41.170 +43:06:36.202' #OBJ 6364
+#coords = '14:38:42.323 +43:08:57.939' #OBJ 7325
 
 c = SkyCoord(coords, unit=(u.hourangle,u.deg))
 
@@ -20,8 +22,26 @@ dec_val = c.dec.value
 min_val = np.inf
 min_ind = None
 
-size = 4200
+good_files = []
 
+for i,file in enumerate(files):
+
+    patch_fits = fits.open(file)[1]
+
+    w = WCS(patch_fits.header)
+
+    ra_ind, dec_ind = w.world_to_pixel(c)
+
+    ra_flag = (ra_ind>0) & (ra_ind<patch_fits.header['NAXIS1'])
+    dec_flag = (dec_ind>0) & (dec_ind<patch_fits.header['NAXIS2'])
+
+    if ra_flag & dec_flag:
+        good_files.append(file)
+
+
+print(good_files)
+
+'''
 for i,file in enumerate(files):
 
     patch_fits = fits.open(file)[1]
@@ -47,7 +67,8 @@ for i,file in enumerate(files):
 
         min_ind = i
         min_val = dist
-
-embed()
-
+        
 print(files[min_ind])
+
+'''
+
